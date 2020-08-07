c5b5707de04e4256207e429e27bbab8436dbbbf7
Ramiro Estrella <balance3840@hotmail.com>
2020-08-07 19:58:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World MOD!');
+  res.send('Hello World MOD 2!');
 });
 
 var server = app.listen(3000, function () {
